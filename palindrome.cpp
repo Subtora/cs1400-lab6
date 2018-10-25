a4b0e4f1c62adbbba26fa3cacaf9facce71042c7
Trevor <16tsmith55@gmail.com>
2018-10-25 14:40:17 -0600

    added string cin and out along with rem of spaces

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <string>
+
+void cinFullString(std::string &s);
+void removeSpaces(std::string &s);
 
 int main() {
+	std::string s;
+	cinFullString(s);
+	removeSpaces(s);
+
+	std::cout << s << std::endl;
+	std::cin.get();
 	return 0;
+}
+
+void cinFullString(std::string &s) {
+	getline(std::cin, s);
+}
+void removeSpaces(std::string &s) {
+	for (int i = 0; i < s.length(); i++)
+		while (s[i] == ' ')
+			s.erase(i, 1);
 }
