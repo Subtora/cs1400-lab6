22e13996af31f90e5cc61df442343b86162d77fd
Trevor <16tsmith55@gmail.com>
2018-10-26 10:38:33 -0600

    attempt to fix & reupload critical files

--- palindrome.cpp
+++ palindrome.cpp
@@ -7,7 +7,7 @@ std::string removeSpaces(std::string s);
 std::string reverse(std::string s);
 bool isPalindrome(std::string a);
 
-
+#if 0
 int main() {
 	std::string s;
 	cinFullString(s);
@@ -19,7 +19,7 @@ int main() {
 
 	return 0;
 }
-
+#endif 
 void cinFullString(std::string &s) {
 	getline(std::cin, s);
 }
